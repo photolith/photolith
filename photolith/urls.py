364ef5c77485ab9dcbd92dd44843a9bb9d4e49c0
Jamie Lentin <jm@lentin.co.uk>
2023-05-23 14:06:09 +0000

    ingest: Add ingest endpoint

--- photolith/urls.py
+++ photolith/urls.py
@@ -20,4 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("photolith.home.urls", namespace="home")),
+    path("ingest/", include("photolith.ingest.urls", namespace="ingest")),
 ]
