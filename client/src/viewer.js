7b918dabb4e6266e918610664ab1b46c54a93cd2
Jamie Lentin <jm@lentin.co.uk>
2023-06-06 09:37:00 +0000

    client/viewer: Add opencv.js image transform pipeline
    Manage image objects to do reasonably quick transforms, outside the DOM
    so they can be moved to a worker if required.
    
    The slowest operation currently is BitmapImage -> ImageData, so only do
    this when we really have to, and only do it once per image.

--- client/src/viewer.js
+++ client/src/viewer.js
@@ -33,65 +33,173 @@ class PhViewer {
   }
 
   redraw () {
-    const ctx = this.elCanvas.getContext('2d');
+    return this.rerender().then((image) => {
+      if (!image) return;
 
-    if (this.elCanvas.width * this.zoom > this.image.width) {
-      // Image smaller than canvas, switch around logic
-      if (this.offsetX < 0) {
+      if (this.elCanvas.width * this.zoom > image.width) {
+        // Image smaller than canvas, switch around logic
+        if (this.offsetX < 0) {
+          this.offsetX = 0;
+        } else if (this.elCanvas.width * this.zoom - this.offsetX < image.width) {
+          this.offsetX = this.elCanvas.width * this.zoom - image.width;
+        }
+      } else if (this.offsetX >= 0) {
         this.offsetX = 0;
-      } else if (this.elCanvas.width * this.zoom - this.offsetX < this.image.width) {
-        this.offsetX = this.elCanvas.width * this.zoom - this.image.width;
+      } else if (this.elCanvas.width * this.zoom - this.offsetX > image.width) {
+        this.offsetX = this.elCanvas.width * this.zoom - image.width;
       }
-    } else if (this.offsetX >= 0) {
-      this.offsetX = 0;
-    } else if (this.elCanvas.width * this.zoom - this.offsetX > this.image.width) {
-      this.offsetX = this.elCanvas.width * this.zoom - this.image.width;
-    }
-    if (this.elCanvas.height * this.zoom > this.image.height) {
-      // Image smaller than canvas, switch around logic
-      if (this.offsetY < 0) {
+      if (this.elCanvas.height * this.zoom > image.height) {
+        // Image smaller than canvas, switch around logic
+        if (this.offsetY < 0) {
+          this.offsetY = 0;
+        } else if (this.elCanvas.height * this.zoom - this.offsetY < image.height) {
+          this.offsetY = this.elCanvas.height * this.zoom - image.width;
+        }
+      } else if (this.offsetY >= 0) {
         this.offsetY = 0;
-      } else if (this.elCanvas.height * this.zoom - this.offsetY < this.image.height) {
-        this.offsetY = this.elCanvas.height * this.zoom - this.image.width;
+      } else if (this.elCanvas.height * this.zoom - this.offsetY > image.height) {
+        this.offsetY = this.elCanvas.height * this.zoom - image.height;
       }
-    } else if (this.offsetY >= 0) {
-      this.offsetY = 0;
-    } else if (this.elCanvas.height * this.zoom - this.offsetY > this.image.height) {
-      this.offsetY = this.elCanvas.height * this.zoom - this.image.height;
-    }
 
-    ctx.clearRect(0, 0, this.elCanvas.width, this.elCanvas.height);
-    if (!this.image) return;
-    ctx.drawImage(
-      this.image,
-      // Top-left of source image
-      0 - this.offsetX,
-      0 - this.offsetY,
-      // W/H to extract from source image
-      this.elCanvas.width * this.zoom,
-      this.elCanvas.height * this.zoom,
-      // Top-left of destination in canvas
-      0,
-      0,
-      // W/H of destination in canvas
-      this.elCanvas.width,
-      this.elCanvas.height);
+      const ctx = this.elCanvas.getContext('2d');
+      ctx.clearRect(0, 0, this.elCanvas.width, this.elCanvas.height);
+      ctx.drawImage(
+        image,
+        // Top-left of source image
+        0 - this.offsetX,
+        0 - this.offsetY,
+        // W/H to extract from source image
+        this.elCanvas.width * this.zoom,
+        this.elCanvas.height * this.zoom,
+        // Top-left of destination in canvas
+        0,
+        0,
+        // W/H of destination in canvas
+        this.elCanvas.width,
+        this.elCanvas.height);
+    });
   }
 
-  load (f) {
-    return new Promise((resolve) => {
-      const reader = new window.FileReader();
+  rerender () {
+    // converts a cv.Mat into imageData (i.e. the guts of imshow)
+    function imageDataFromMat (cv, mat) {
+      const img = new cv.Mat();
+      const depth = mat.type() % 8;
+      const scale =
+        depth <= cv.CV_8S ? 1.0 : depth <= cv.CV_32S ? 1.0 / 256.0 : 255.0;
+      const shift = depth === cv.CV_8S || depth === cv.CV_16S ? 128.0 : 0.0;
+      mat.convertTo(img, cv.CV_8U, scale, shift);
 
-      this.image = new window.Image();
-      this.image.onload = (e) => {
-        this.setOffset(0, 0, this.image.width / this.elCanvas.width);
-        this.redraw();
-        resolve();
-      };
+      // converts the img type to cv.CV_8UC4
+      switch (img.type()) {
+        case cv.CV_8UC1:
+          cv.cvtColor(img, img, cv.COLOR_GRAY2RGBA);
+          break;
+        case cv.CV_8UC3:
+          cv.cvtColor(img, img, cv.COLOR_RGB2RGBA);
+          break;
+        case cv.CV_8UC4:
+          break;
+        default:
+          throw new Error(
+            'Bad number of channels (Source image must have 1, 3 or 4 channels)'
+          );
+      }
+      const clampedArray = new window.ImageData(
+        new Uint8ClampedArray(img.data),
+        img.cols,
+        img.rows
+      );
+      img.delete();
+      return clampedArray;
+    }
 
-      // Load file into Image
-      reader.onload = (e) => { this.image.src = e.target.result; };
-      reader.readAsDataURL(f);
+    // Convert a Bitmap object to ImageData by drawing on a temporary canvas
+    function bitmapToImageData (bitmap) {
+      // NB: This is convoluted, but the current way to do it:
+      //     https://github.com/whatwg/html/issues/4785
+      const canvas = new window.OffscreenCanvas(bitmap.width, bitmap.height);
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(bitmap, 0, 0);
+      return ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
+
+    const transforms = this.readTransforms();
+    const cv = window.cv;
+
+    // If nothing to do, resolve immediately with bitmap
+    if (!this.origImage) return Promise.resolve(undefined);
+    if (this.tformedBitmap) return Promise.resolve(this.tformedBitmap);
+
+    // If no transforms, output ImageBitmap is same as input ImageData
+    if (Object.keys(transforms).length === 0) {
+      if (this.origImage instanceof window.ImageBitmap) {
+        return Promise.resolve(this.origImage);
+      }
+      if (this.origImage instanceof window.ImageData) {
+        // Turn ImageData into an ImageBitmap for rendering
+        return window.createImageBitmap(this.origImage).then((bitmap) => {
+          if (this.tformedBitmap) this.tformedBitmap.close();
+          this.tformedBitmap = bitmap;
+          return this.tformedBitmap;
+        });
+      }
+      throw new Error('Unknown origImage type');
+    }
+
+    // this.origImage has to be ImageData before proceeding
+    if (this.origImage instanceof window.ImageBitmap) {
+      const data = bitmapToImageData(this.origImage);
+      this.origImage.close();
+      this.origImage = data;
+    }
+
+    // Generate cvMat of original image
+    const cvMat = cv.matFromImageData(this.origImage);
+    try {
+      if (transforms.equalize) {
+        cv.cvtColor(cvMat, cvMat, cv.COLOR_RGBA2GRAY, 0);
+        cv.equalizeHist(cvMat, cvMat);
+      }
+      if (transforms.canny) {
+        cv.Canny(cvMat, cvMat, 50, 100, 3, false);
+      }
+      return window.createImageBitmap(imageDataFromMat(cv, cvMat)).then((bitmap) => {
+        if (this.tformedBitmap) this.tformedBitmap.close();
+        this.tformedBitmap = bitmap;
+        return this.tformedBitmap;
+      });
+    } catch (error) {
+      console.error(error);
+      throw cv.exceptionFromPtr(error).msg;
+    } finally {
+      cvMat.delete();
+    }
+  }
+
+  readTransforms () {
+    const transforms = window.transforms || {};
+    const transformsString = JSON.stringify(transforms);
+
+    if (this.prevTransforms !== transformsString) {
+      // If transform string changed, invalidate transformed bitmap
+      if (this.tformedBitmap) this.tformedBitmap.close();
+      this.tformedBitmap = undefined;
+    }
+    this.prevTransforms = transformsString;
+    return transforms;
+  }
+
+  load (blob) {
+    return window.createImageBitmap(blob).then((origBitmap) => {
+      if (this.origImage instanceof window.ImageData) this.origImage.close();
+      if (this.tformedBitmap instanceof window.ImageData) this.tformedBitmap.close();
+
+      // NB: origImage starts off as an ImageBitmap until we need an ImageData
+      this.origImage = origBitmap;
+      this.tformedBitmap = undefined;
+      this.setOffset(0, 0, origBitmap.width / this.elCanvas.width);
+      return this.redraw();
     });
   }
 }
