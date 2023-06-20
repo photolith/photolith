8b7e22a9b057bf9935ddbd3bbefa399e6289e5ed
Jamie Lentin <jm@lentin.co.uk>
2023-06-20 17:15:14 +0000

    client/ingest_select: Support ServerFileSet #11
    Adapter to use the ServerFileSets created by the previous commit.

--- client/src/ingest_select.js
+++ client/src/ingest_select.js
@@ -1,4 +1,5 @@
 import { loadFile } from './viewer';
+import { parse } from 'content-disposition-header';
 
 class Cancelled extends Error { }
 
@@ -45,11 +46,46 @@ class LocalFileSet {
   }
 }
 
+class ServerFileSet {
+  constructor (photoDir) {
+    this.photoDir = photoDir;
+    this.prev = null;
+  }
+
+  close () { }
+
+  next () {
+    const url = `/ingest/next-photo/${this.photoDir}/${this.prev ? '?prev=' + this.prev : ''}`;
+    return window.fetch(url).then((resp) => {
+      const remaining = parseInt(resp.headers.get('X-Photolith-Remaining') || 0, 10);
+
+      if (resp.status === 204) {
+        return { f: null, remaining: remaining };
+      }
+      if (resp.status === 200) {
+        return resp.blob().then((blob) => {
+          try {
+            blob.name = parse(resp.headers.get('Content-Disposition')).parameters.filename;
+          } catch (e) {
+            console.warn(`Could not parse Content-Disposition: '${resp.headers.get('Content-Disposition')}'`);
+            blob.name = 'unknown.jpg';
+          }
+          this.prev = blob.name;
+          return { f: blob, remaining: remaining };
+        });
+      }
+      console.error('Failed to fetch next image', resp);
+      throw new Error('Failed to fetch next image');
+    });
+  }
+}
+
 function newFileSet (val) {
   val = val.split(':');
 
   if (val[0] === 'null') return new NullFileSet();
   if (val[0] === 'fileselect') return new LocalFileSet();
+  if (val[0] === 'server') return new ServerFileSet(val[1]);
   throw new Error('Unknown select type ' + val.join(':'));
 }
 
