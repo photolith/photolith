photolith/photolith

commit 193320f198a8d17396f83b44b4fcaf7ce008914e

client/viewer: Load images via. DOM event
Don't scrawl objects on DOM elements, communicate via. events.

client/src/viewer.js
@@ -4,7 +4,6 @@ const formson = require('formson');
 class PhViewer {
   constructor (elViewer) {
     this.elViewer = elViewer;
-    elViewer.phViewer = this;
     this.elForm = this.elViewer.querySelector(':scope form');
 
     if (fabric.isWebglSupported()) {
@@ -119,6 +118,10 @@ class PhViewer {
       if (this.formChangeTimeout) clearTimeout(this.formChangeTimeout);
       this.formChangeTimeout = setTimeout(this.refreshFilters.bind(this), 10);
     };
+
+    this.elViewer.addEventListener('load_file', (event) => {
+      this.load(event.detail.file);
+    });
   }
 
   refreshFilters () {
@@ -254,6 +257,12 @@ class PhCropper extends PhViewer {
       }
     });
   }
+}
+
+export function loadFile (elViewer, f) {
+  elViewer.dispatchEvent(new window.CustomEvent('load_file', {
+    detail: { file: f }
+  }));
 }
 
 export function init (window) {

client/src/ingest_select.js
@@ -1,3 +1,5 @@
+import { loadFile } from './viewer';
+
 class Cancelled extends Error { }
 
 class NullFileSet {
@@ -51,7 +53,7 @@ function newFileSet (val) {
   throw new Error('Unknown select type ' + val.join(':'));
 }
 
-function nextSelection (elSelect, phViewer) {
+function nextSelection (elSelect, elViewer) {
   return elSelect.fs.next().then(({ f = null, remaining = 0 }) => {
     if (!elSelect.options[0].phOrigText) elSelect.options[0].phOrigText = elSelect.options[0].text;
 
@@ -60,7 +62,7 @@ function nextSelection (elSelect, phViewer) {
     } else {
       elSelect.options[0].text = elSelect.options[0].phOrigText;
     }
-    phViewer.load(f); // NB: If null will unload image
+    loadFile(elViewer, f); // NB: If null will unload image
   }).catch((err) => {
     if (err instanceof Cancelled) {
       // File select cancelled, don't change anything.
@@ -82,12 +84,12 @@ export function init (window) {
       if (elSelect.fs) elSelect.fs.close();
       elSelect.fs = newFileSet(elSelect.value);
       elSelect.selectedIndex = 0;
-      nextSelection(elSelect, elViewer.phViewer);
+      nextSelection(elSelect, elViewer);
     });
 
     elNextButton.addEventListener('click', (event) => {
       event.preventDefault();
-      nextSelection(elSelect, elViewer.phViewer);
+      nextSelection(elSelect, elViewer);
     });
   });
 }

commit 2e6a231f0f5f044c39a2b711b270b7c84feebfda

templates/ingest: Use DOM event to shiftBoundingBox
Was broken by 193320f, similarly, wire it up with a DOM event.

client/src/viewer.js
@@ -221,6 +221,10 @@ class PhCropper extends PhViewer {
   constructor (elViewer) {
     super(elViewer);
     this.fabCanvas.uniformScaling = false; // Don't try to preserve aspect-ratio when resizing rects
+
+    this.elViewer.addEventListener('shift_bb', (event) => {
+      this.shiftBoundingBox();
+    });
   }
 
   boundingBox () {

photolith/templates/ingest/index.html
@@ -47,7 +47,7 @@
 
     <div class="row">
       <div class="col" style="text-align: right;">
-        <button class="btn btn-success" onclick="document.querySelector('#image-viewer').phViewer.shiftBoundingBox() ; form.individual.value = isFinite(parseInt(form.individual.value, 10)) ? parseInt(form.individual.value, 10) + 1 : 1">{% translate "Save & Next" %}</button>
+        <button class="btn btn-success" onclick="document.querySelector('#image-viewer').dispatchEvent(new window.CustomEvent('shift_bb')) ; form.individual.value = isFinite(parseInt(form.individual.value, 10)) ? parseInt(form.individual.value, 10) + 1 : 1">{% translate "Save & Next" %}</button>
       </div>
     </div>
   </form>