885d1687eabc0f03949ca55a23c180bb825e84e7
Jamie Lentin <jm@lentin.co.uk>
2023-06-20 17:11:13 +0000

    ingest/photo_dir: Server machinations to fetch images #11
    Module representing a set of directories with photos in it, allow
    fetching of photos in date order.

---
+++ photolith/ingest/photo_dir.py
@@ -0,0 +1,45 @@
+import mimetypes
+import os
+import os.path
+import pathlib
+import re
+
+
+def list_photo_dirs(base):
+    for p in sorted(pathlib.Path(base).iterdir()):
+        if p.is_dir():
+            yield os.path.basename(p)
+
+
+def get_next_photo(base, photo_dir, prev=None):
+    dir_path = pathlib.Path(base) / photo_dir
+    if not re.match(r"^\w+$", photo_dir) or not os.path.exists(dir_path):
+        raise ValueError("Invalid photo_dir '%s'" % photo_dir)
+
+    file_path = None
+    remaining = 0
+    for p in sorted(dir_path.iterdir(), key=os.path.getmtime):
+        if not p.is_file():
+            # Ignore directories
+            pass
+        elif prev:
+            if os.path.basename(p) == prev:
+                # Found previous entry, stop looking for it & return next file
+                prev = None
+        elif file_path is None:
+            # Return first file we found (but keep counting)
+            file_path = p
+        else:
+            # Count anything else left
+            remaining += 1
+
+    if file_path is None:
+        return None
+
+    # Open file, removing it now it's been handed clientside
+    return dict(
+        path=file_path,
+        name=os.path.basename(file_path),
+        mime=mimetypes.guess_type(file_path)[0],
+        remaining=remaining,
+    )
---
+++ photolith/tests/test_ingest_photo_dir.py
@@ -0,0 +1,94 @@
+import os
+import pathlib
+import tempfile
+import shutil
+
+from django.test import TestCase
+
+from photolith.ingest.photo_dir import list_photo_dirs, get_next_photo
+
+
+class PhotoDirTestCase(TestCase):
+    def setUp(self):
+        super(PhotoDirTestCase, self).setUp()
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.base_path = pathlib.Path(self.temp_dir.name)
+
+    def tearDown(self):
+        shutil.rmtree(self.base_path)
+        super(PhotoDirTestCase, self).tearDown()
+
+    def create_file(self, file_path):
+        with open(file_path, "wb") as f:
+            f.write(b"#")
+
+    def test_list_photo_dirs(self):
+        def lpd():
+            return list(list_photo_dirs(self.base_path))
+
+        # Starts empty
+        self.assertEqual(lpd(), [])
+
+        # Output sorted
+        os.makedirs(self.base_path / "z_cuthbert")
+        os.makedirs(self.base_path / "a_dibble")
+        self.assertEqual(lpd(), ["a_dibble", "z_cuthbert"])
+
+        # Files ignored
+        self.create_file(self.base_path / "f_file")
+        self.assertEqual(lpd(), ["a_dibble", "z_cuthbert"])
+
+    def test_get_next_photo(self):
+        def gnp(photo_dir, prev=None):
+            return get_next_photo(self.base_path, photo_dir, prev)
+
+        os.makedirs(self.base_path / "pd")
+        os.makedirs(self.base_path / "pd" / "nested")
+        os.makedirs(self.base_path / "pd2")
+
+        # Invalid photoDirs
+        with self.assertRaises(ValueError):
+            gnp("non-existant")
+        with self.assertRaises(ValueError):
+            gnp("..")
+        with self.assertRaises(ValueError):
+            gnp("pd/nested")
+
+        # Empty directory, nothing to find
+        self.assertEqual(gnp("pd"), None)
+        self.assertEqual(gnp("pd", prev="001.jpg"), None)
+        self.assertEqual(gnp("pd2"), None)
+
+        # Create files, returned in order
+        self.create_file(self.base_path / "pd" / "001.jpg")
+        self.create_file(self.base_path / "pd" / "002.png")
+        self.create_file(self.base_path / "pd" / "003.jpg")
+        self.assertEqual(
+            gnp("pd"),
+            dict(
+                path=self.base_path / "pd" / "001.jpg",
+                name="001.jpg",
+                mime="image/jpeg",
+                remaining=2,
+            ),
+        )
+        self.assertEqual(
+            gnp("pd", prev="001.jpg"),
+            dict(
+                path=self.base_path / "pd" / "002.png",
+                name="002.png",
+                mime="image/png",
+                remaining=1,
+            ),
+        )
+        self.assertEqual(
+            gnp("pd", prev="002.png"),
+            dict(
+                path=self.base_path / "pd" / "003.jpg",
+                name="003.jpg",
+                mime="image/jpeg",
+                remaining=0,
+            ),
+        )
+        self.assertEqual(gnp("pd", prev="003.jpg"), None)
+        self.assertEqual(gnp("pd2"), None)
