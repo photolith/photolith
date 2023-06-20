23fdb302e0786b53f4f95632455d86dd20498a59
Jamie Lentin <jm@lentin.co.uk>
2023-06-20 17:12:47 +0000

    ingest/views: Server photo endpoints #11
    List all photo dirs in as valid inputs, add endpoint to fetch images.

--- photolith/ingest/urls.py
+++ photolith/ingest/urls.py
@@ -8,4 +8,7 @@ app_name = "photolith"
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name="index"),
+    path(
+        "next-photo/<slug:photo_dir>/", views.NextPhotoView.as_view(), name="next-photo"
+    ),
 ]
--- photolith/ingest/views.py
+++ photolith/ingest/views.py
@@ -1,7 +1,14 @@
+import os
+
+from django.conf import settings
+from django.http import FileResponse, HttpResponse
 from django.shortcuts import render
 from django.utils.translation import ngettext
 from django.utils.translation import gettext as _
+from django.views import View
 from django.views.generic import TemplateView
+
+from .photo_dir import get_next_photo, list_photo_dirs
 
 
 class IndexView(TemplateView):
@@ -13,9 +20,44 @@ class IndexView(TemplateView):
                 count=count,
             )
 
+        for d in list_photo_dirs(settings.INGEST_ROOT):
+            yield dict(
+                name="server:%s" % d,
+                description=_("Uploaded by %(photo_dir)s") % dict(photo_dir=d),
+            )
         yield dict(name="fileselect:", description=_("Upload file from computer"))
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["image_sources"] = self.image_sources()
         return context
+
+
+class NextPhotoView(View):
+    def get(self, *args, **kwargs):
+        f = get_next_photo(
+            settings.INGEST_ROOT,
+            kwargs["photo_dir"],
+            prev=self.request.GET.get("prev", None),
+        )
+        if f is None:
+            return HttpResponse(
+                None,
+                status=204,
+                headers={
+                    "X-Photolith-Remaining": 0,
+                    "Cache-Control": "no-cache, no-store, must-revalidate",
+                    "Pragma": "no-cache",
+                    "Expires": 0,
+                },
+            )
+        return FileResponse(
+            open(f["path"], "rb"),
+            headers={
+                "X-Photolith-Remaining": f["remaining"],
+                "Content-Type": f["mime"],
+                "Cache-Control": "no-cache, no-store, must-revalidate",
+                "Pragma": "no-cache",
+                "Expires": 0,
+            },
+        )
