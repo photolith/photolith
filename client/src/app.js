1db05083164b8888f80f634ca89f30d03455a643
Jamie Lentin <jm@lentin.co.uk>
2023-06-26 16:55:08 +0000

    client/app: Global error alerts
    Add mechanism to show errors to user.

--- client/src/app.js
+++ client/src/app.js
@@ -5,3 +5,27 @@ window.addEventListener('DOMContentLoaded', (event) => {
   initIngestSelect(window);
   initViewer(window);
 });
+
+function displayAlert (level, messageHTML) {
+  const elAlert = document.createElement('DIV');
+
+  elAlert.className = `alert alert-${level} alert-dismissible fade show`;
+  elAlert.setAttribute('role', 'alert');
+  elAlert.innerHTML = [
+    messageHTML,
+    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>'
+  ].join('\n');
+  document.getElementById('alert-container').append(elAlert);
+
+  window.setTimeout(() => {
+    if (elAlert.isConnected) new window.bootstrap.Alert(elAlert).close();
+  }, 5000);
+}
+
+window.addEventListener('unhandledrejection', (event) => {
+  displayAlert('danger', event.reason);
+});
+
+window.onerror = (event, source, lineno, colno, error) => {
+  displayAlert('danger', error);
+};
