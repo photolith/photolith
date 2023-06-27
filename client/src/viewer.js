6cc47b84c1ef2f54633c4cceb9f4b800e4de410f
Jamie Lentin <jm@lentin.co.uk>
2023-06-27 14:17:47 +0000

    client/viewer: Image download link #21

--- client/src/viewer.js
+++ client/src/viewer.js
@@ -132,6 +132,10 @@ class PhViewer {
     };
 
     this.elViewer.addEventListener('load_file', (event) => {
+      const elDl = this.elViewer.querySelector(':scope .download-link');
+
+      elDl.href = event.detail.file ? URL.createObjectURL(event.detail.file) : '#';
+      elDl.download = event.detail.file ? event.detail.file.name : undefined;
       this.load(event.detail.file);
     });
   }
@@ -334,6 +338,13 @@ export function init (window) {
               <label class="form-check-label" for="${idPrefix}-laplace-input">Edge detection</label>
             </div>
             <div class="text-end px-1">
+              <a href="#" class="download-link btn btn-sm btn-secondary">
+                <!-- https://icons.getbootstrap.com/icons/download/ -->
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-download" viewBox="0 0 16 16">
+                  <path d="M.5 9.9a.5.5 0 0 1 .5.5v2.5a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.5a.5.5 0 0 1 1 0v2.5a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2v-2.5a.5.5 0 0 1 .5-.5z"/>
+                  <path d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z"/>
+                </svg>
+              </a>
               <input type="reset" class="btn btn-sm btn-secondary" value="Reset" />
             </div>
           </form>
