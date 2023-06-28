b8608b560a0d1b16d1fea28c7aa89a88dd0a0608
Jamie Lentin <jm@lentin.co.uk>
2023-06-28 15:58:45 +0000

    client/metadata_api/dummy: Translated random values for dummy input
    Choose sex & maturity from a fixed translated vocab based on MFDB.

--- client/src/metadata_api/dummy.js
+++ client/src/metadata_api/dummy.js
@@ -3,6 +3,23 @@ const species = [
   { id: 2, en: 'Greenland Halibut [GLH]', is: 'Grálúða [GLH]' }
 ];
 
+const sex = [
+  { id: 1, en: 'Male [M]', is: 'Karlkyns [M]' },
+  { id: 2, en: 'Female [F]', is: 'Kvenkyns [F]' },
+  { id: 3, en: 'Mixed [X]', is: 'Blandað [X]' },
+  { id: 4, en: 'Indeterminate [N]', is: 'Óákveðið [N]' },
+  { id: 4, en: 'Unknown [U]', is: 'Óþekktur [U]' }
+];
+
+const maturity = [
+  { id: 1, en: 'Immature', is: 'Óþroskaður' },
+  { id: 5, en: 'Mature', is: 'Þroskaður' }
+];
+
+function randomChoice (ar) {
+  return ar[Math.floor(Math.random() * ar.length)];
+}
+
 export default class MetadataApi {
   sampleDetail (sampleId) {
     const intSampleId = parseInt(sampleId, 10);
@@ -29,9 +46,9 @@ export default class MetadataApi {
           measureId
         ].join(' '),
         length: Math.floor(Math.random() * 100),
-        sex: Math.floor(Math.random() * 2),
-        maturity: Math.floor(Math.random() * 2),
-        species: species[Math.floor(Math.random() * species.length)]
+        sex: randomChoice(sex),
+        maturity: randomChoice(maturity),
+        species: randomChoice(species)
       };
     });
   }
