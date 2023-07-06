3e8f3b4ff3fa30db5d474a6c5752ef7e7f874fc9
Jamie Lentin <jm@lentin.co.uk>
2023-07-06 14:20:21 +0000

    client/search: Add image preview
    Clickable child rows that show the image, cropped to the boundingBox.

--- client/src/search.js
+++ client/src/search.js
@@ -3,10 +3,50 @@ import DataTable from 'datatables.net-bs5';
 
 import { renderMetaLabel, renderMetaCell } from './meta';
 
+function croppedImageViewer (href, boundingBox, canvasStyle) {
+  const elCanvas = document.createElement('CANVAS');
+  const elImage = new window.Image();
+
+  elCanvas.setAttribute('style', canvasStyle || '');
+
+  // Size canvas to natural size of cropped area (CSS will worry about scaling)
+  elCanvas.width = boundingBox[1][0] - boundingBox[0][0];
+  elCanvas.height = boundingBox[1][1] - boundingBox[0][1];
+
+  elImage.onload = (e) => {
+    const ctx = elCanvas.getContext('2d');
+    ctx.drawImage(
+      elImage,
+      // Top-left of source image
+      boundingBox[0][0],
+      boundingBox[0][1],
+      // W/H to extract from source image
+      elCanvas.width,
+      elCanvas.height,
+      // Top-left of destination in canvas
+      0,
+      0,
+      // W/H of destination in canvas
+      elCanvas.width,
+      elCanvas.height
+    );
+    elImage.src = '';
+  };
+  elImage.src = href;
+
+  return elCanvas;
+}
+
 export function init (window) {
   window.document.querySelectorAll('.ph-search-table').forEach((elSearchTable) => {
     const lang = document.documentElement.lang || 'en';
     const metaLabels = window.mApi.metaLabels(lang);
+
+    function childRow (row) {
+      const el = document.createElement('DIV');
+      el.append(croppedImageViewer(row.image, row.bounding_box, 'height: 300px;'));
+      return el;
+    }
 
     // https://datatables.net/reference/option/%24.fn.dataTable.ext.errMode
     DataTable.ext.errMode = 'throw';
@@ -24,6 +64,22 @@ export function init (window) {
         };
       }),
       searching: false
+    });
+
+    table.on('click', 'td', function (e) {
+      // https://datatables.net/reference/api/row().child
+      const row = table.row(e.target.closest('tr'));
+
+      if (!row.child() || row.child().length === 0) {
+        row.child(childRow(row.data()));
+      }
+
+      // Open/close row
+      if (row.child.isShown()) {
+        row.child.hide();
+      } else {
+        row.child.show();
+      }
     });
   });
 }
