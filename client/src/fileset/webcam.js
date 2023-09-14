bdb3c0900ddbe360c194fa1a04bc89e2ab17765f
Jamie Lentin <jm@lentin.co.uk>
2023-09-14 17:00:48 +0000

    client/fileset: Allow camera selection #40
    Add every available camera to the ingest-select dropdown, so it's
    possible to choose a non-default camera, e.g. a USB microscope rather
    than your laptop's webcam.
    
    Annoyingly the enumerateDevices() list is only available after at least
    one camera has been successfully opened, so there's no way of
    pre-populating the list. To get around this, bodge the select list
    after we've opened the default camera once to add additional options.

--- client/src/fileset/webcam.js
+++ client/src/fileset/webcam.js
@@ -4,9 +4,29 @@ import { Cancelled } from '../errors';
 // https://developer.mozilla.org/en-US/docs/Web/API/MediaStream
 
 export class WebcamFileSet {
-  constructor () {
+  constructor (deviceId) {
     this.name = 'webcam:';
     this.elViewer = window.document.querySelector('.ph-viewer');
+    this.deviceId = deviceId || undefined;
+  }
+
+  updateSelectDevices () {
+    const elSelect = window.document.querySelector('.ph-ingest-select-bar select');
+    const webcamOpts = Array.from(elSelect.options).filter((o) => o.value.startsWith(this.name));
+
+    // Only add options once
+    if (webcamOpts.length > 1) return;
+
+    // NB: We can't do this until at least one camera is opened, so we have to bodge it after the first open:
+    //     https://github.com/w3c/mediacapture-main/issues/874
+    navigator.mediaDevices.enumerateDevices().then((devices) => devices.forEach((device) => {
+      if (device.kind === 'videoinput') {
+        elSelect.appendChild(new window.Option(
+          webcamOpts[0].textContent.replace(/\(.*\)$/, '(' + device.label + ')'),
+          this.name + device.deviceId
+        ));
+      }
+    }));
   }
 
   close () {
@@ -22,13 +42,15 @@ export class WebcamFileSet {
       if (this.video) return;
 
       // Open a new video stream
-      return navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } }).then((stream) => {
+      return navigator.mediaDevices.getUserMedia({ video: { deviceId: this.deviceId, facingMode: 'environment' } }).then((stream) => {
         this.video = document.createElement('VIDEO');
         this.video.name = stream.getVideoTracks()[0].label; // NB: So we can later extract a filename from the element
         this.video.setAttribute('style', this.elViewer.querySelector(':scope>.canvas-container').getAttribute('style'));
         this.video.style.display = 'none';
         this.video.srcObject = stream;
         this.elViewer.appendChild(this.video);
+
+        this.updateSelectDevices();
       });
     }).then(() => {
       // Start video and wait for user to take photo
