edc0fe769798a2f8b34ba8dc950abc2f50193abf
Jamie Lentin <jm@lentin.co.uk>
2023-09-28 10:56:22 +0000

    templates: Default error page templates

--- photolith/urls.py
+++ photolith/urls.py
@@ -13,8 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from django.views import defaults as default_views
 
 
 urlpatterns = [
@@ -28,3 +30,26 @@ urlpatterns = [
     path("media/", include("photolith.media.urls", namespace="media")),
     path("search/", include("photolith.search.urls", namespace="search")),
 ]
+
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url (e.g 404) in browser to see how these error pages look like.
+    urlpatterns += [
+        path(
+            r"400/",
+            default_views.bad_request,
+            kwargs={"exception": Exception("Bad Request!")},
+        ),
+        path(
+            r"403/",
+            default_views.permission_denied,
+            kwargs={"exception": Exception("Permission Denied")},
+        ),
+        path(
+            r"404/",
+            default_views.page_not_found,
+            kwargs={"exception": Exception("Page not Found")},
+        ),
+        path(r"500/", default_views.server_error),
+    ]
