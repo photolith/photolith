b4c5cd3663a3fa8becc7662c53d82ccda6ad8a65
Jamie Lentin <jm@lentin.co.uk>
2023-11-15 12:14:26 +0000

    admin: Remove permissions field
    We should only assign permissions via. groups, remove the tempting
    list.

--- photolith/admin.py
+++ photolith/admin.py
@@ -68,6 +68,22 @@ class UserAdmin(BaseUserAdmin):
     actions = [user_activate]
 
     inlines = [UserProfileInline, TeamInline]
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
     add_fieldsets = (
         (
             None,
