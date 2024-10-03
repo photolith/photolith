53301c87b8b11d16db00afecb38bfaee7c3312f5
Jamie Lentin <jm@lentin.co.uk>
2024-10-03 15:45:12 +0100

    preinstall: Use debian-provided node/npm
    Debian provides it's own nowadays, use that to save complications.

--- preinstall.sh
+++ preinstall.sh
@@ -5,14 +5,6 @@ set -eu
 apt-get $* update
 apt-get $* install make curl
 
-# Add APT source for node
-cat <<EOF > /etc/apt/sources.list.d/nodesource.list
-# curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key -o /usr/share/keyrings/nodesource.asc
-deb [signed-by=/usr/share/keyrings/nodesource.asc] https://deb.nodesource.com/node_18.x ${VERSION_CODENAME} main
-deb-src [signed-by=/usr/share/keyrings/nodesource.asc] https://deb.nodesource.com/node_18.x ${VERSION_CODENAME} main
-EOF
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key -o /usr/share/keyrings/nodesource.asc
-apt-get $* update
 
 # Server dependencies
 apt-get $* install \
@@ -22,4 +14,4 @@ apt-get $* install \
 
 # Client dependencies
 apt-get $* install \
-    nodejs
+    nodejs npm
