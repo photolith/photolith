cf46d8830d5ecfc93275463ddfbeb0ce98955b39
Jamie Lentin <jm@lentin.co.uk>
2024-11-08 15:37:40 +0000

    docker: Destroy & rebuild staticfiles on startup
    When starting a new image, the static volume may already exist. If so
    we need to repopulate it with latest content.
    
    To be able to do this, the app user needs to be able to write to the
    volume, chown the default volume content to app, so subsequent
    collectstatic runs can work.

--- docker/wsgi/entrypoint.sh
+++ docker/wsgi/entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -eu
 
+# Run collectstatic at every startup to ensure static volume is up-to-date
+./manage.py collectstatic --noinput --clear
+
 exec ./bin/gunicorn \
   --workers 3 \
   --bind="0.0.0.0:8000" \
