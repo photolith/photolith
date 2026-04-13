f4725fdc489df8d399a03747ccca0f894466b582
Jamie Lentin <jm@lentin.co.uk>
2026-04-13 16:40:07 +0000

    s6-overlay: Use django for static/media with external WSGI
    For aeb2286 (previous commit) to be useful, we also need static/media
    requests to be served via. django. Otherwise they'll inevitably fail.
    
    This wouldn't work via. bind mounts, as there is none for static &
    media would fail to upload.

--- photolith/urls.py
+++ photolith/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
@@ -53,3 +54,6 @@ if settings.DEBUG:
         ),
         path(r"500/", default_views.server_error),
     ]
+    # https://docs.djangoproject.com/en/6.0/howto/static-files/#serving-static-files-during-development
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
